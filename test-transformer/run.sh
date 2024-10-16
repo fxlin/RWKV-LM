4d47649dad568409985eb70437c5df2db7b74ff2
Wonkyo Choe <wonkyochoe@virginia.edu>
2024-10-16 16:04:35 -0400

    up

--- test-transformer/run.sh
+++ test-transformer/run.sh
@@ -2,11 +2,14 @@
 # an evaluation script for other transformer models
 
 readarray -t models < <(grep -v '#' models.txt)
+models=("JackFram/llama-160m")
 
 for m in "${models[@]}"; do
     echo $m
-    #lm_eval --model hf \
-    #    --model_args pretrained=$m,revision=step100000,dtype="float" \
-    #    --tasks lambada_openai \
-    #    --batch_size 8
+    lm_eval --model hf \
+        --model_args pretrained=$m,dtype="float" \
+        --tasks lambada_openai \
+        --batch_size 8 \
+        --log_samples \
+        --output_path results
 done
